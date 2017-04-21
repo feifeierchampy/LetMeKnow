0b33af8559dfc326e46542fe40f0be673b6449e9
ssd-company2@champy <liuchang112133@gmail.com>
2017-04-21 23:07:30 +0800

    sync code

---
+++ app/src/main/java/com/feifeier/letmeknow/managers/service/SmsListenerService.java
@@ -0,0 +1,21 @@
+package com.feifeier.letmeknow.managers.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class SmsListenerService extends Service {
+    public SmsListenerService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
